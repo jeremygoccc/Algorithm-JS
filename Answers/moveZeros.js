36bda94b3540d6e274d2f26e660e34bc2dd51ec3
“Jeremy” <zhuguogen@qq.com>
2019-03-13 09:48:01 +0800

    modified moveZeros solution

--- Answers/moveZeros.js
+++ Answers/moveZeros.js
@@ -5,21 +5,37 @@
   设定一个临时变量k=0，遍历数组nums，将非零元素与之前的零元素进行交换，维护变量k的值。
  */
 
-const nums = [1, 0, 0, 3, 0, 6, 1]
+const nums = [0, 1, 0, 3, 12]
 
 const moveZeros = (nums) => {
   let k = 0
   for (let i = 0, len = nums.length; i < len; i++) {
-    if (nums[i] !== 0) {
-      if (k !== i) {
-        k++
-        let tmp = nums[i]
-        nums[i] = nums[k]
-        nums[k] = tmp
-      }
+    if (nums[i]) {
+      nums[k] = nums[i]
+      k++
     }
+  }
+  for (let i = k, len = nums.length; i < len; i++) {
+    nums[i] = 0
   }
   return nums
 }
+
+// const moveZeros2 = (nums) => {
+//   let k = 0
+//   for (let i = 0, len = nums.length; i < len; i++) {
+//     if (nums[i]) {
+//       if (k !== i) {
+//         let tmp = nums[i]
+//         nums[i] = nums[k]
+//         nums[k] = tmp
+//         k++
+//       }
+//     } else {
+//       k++
+//     }
+//   }
+//   return nums
+// }
 
 console.log(moveZeros(nums))
