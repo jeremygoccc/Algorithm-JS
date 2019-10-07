8a421f6b8824b1fd7b5cf1938ae4c1622b573067
“Jeremy” <zhuguogen@qq.com>
2019-10-07 16:17:29 +0800

    fix: isSymmetric

--- Answers/Tree/isSymmetric.js
+++ Answers/Tree/isSymmetric.js
@@ -11,5 +11,5 @@ const isSymmetricTwo = (t1, t2) => {
   if (t1 === null && t2 === null) return true
   if (t1 === null || t2 === null) return false
   if (t1.val !== t2.val) return false
-  return isSymmetricTwo(t1.left, t2.right) && isSymmetric(t1.right, t2.left)
+  return isSymmetricTwo(t1.left, t2.right) && isSymmetricTwo(t1.right, t2.left)
 }
